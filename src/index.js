1bf475bd06678dcb05e95f300583e24cb3d60021
max <kodtya.oreshin@inbox.ru>
2023-08-21 19:16:41 +0300

    fixBag

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import 'styles/app.scss';
+import './styles/app.scss';
 import App from './components/App';
 import { BrowserRouter } from 'react-router-dom';
 import AppRouter from './routes/AppRouter';
